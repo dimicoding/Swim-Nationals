7a755d120f22ce9129fb2955bf83a6697515dd9c
Dmytro Martynenko <dmytromartynenko1@gmail.com>
2023-03-03 11:44:00 +0000

    add validation steps to the get_athlete_info function

--- run.py
+++ run.py
@@ -20,34 +20,49 @@ def get_athlete_info():
     Age, last two numbers of birth the birth year.
     Gender, either "m" or "f" letter.
     """
+    print("Before you choose your races, I need some details...")
+    print("Your Name, Year and Gender.\n")
+
     while True:
-        print("Before you choose your races, I need some details...")
-        print("Your Name, Year and Gender.\n")
-        
+        #get users name and validate it
         print("Your name should contain between 3 and 9 letters.")
-        print("Example: Michael P\n")
-
+        print("Example: MichaelP\n")
+        
         name= input("Enter your name here:\n")
-        if name.isalpha(len(name) > 2 and len(name) < 10):
+        if name.isalpha() and len(name) > 2 and len(name) < 10:
             print("Hi! " + name + " what is the year you were born?")
-            
-        else:
-            raise ValueError("Invalid name! Please enter a valid name.")
             break
-
-        print("Type only the two last numbers.")
-        print("Example: 00\n")
-        year= int(input("Your year:\n"))
-        
-
-
-        
+        else:
+            print("Invalid name! Please enter a valid name.")
             
+    while True:
+        #get users birth year and validate it
+        try:
+            print("Type only the two last numbers.")
+            print("Example: 00\n")
+            year= input("Your year:\n")
+            if year.isdigit() and len(year) ==2:
+                year= int(year)
+                print("Year valid.")
+                break
+            else:
+                raise ValueError
+        except ValueError:
+                print("Invalid birth year, enter a valid year")  
+    
+    while True:
+        #get users gender and validate it
+        try:
+            gender = input("Please enter your gender (M/F):\n")
+            if gender.lower() not in ['m', 'f']:
+                raise ValueError
+            print("Recieving...")
+            print(f'Verify if everythin is correct: name:"{name}"year:"{year}"gender:"{gender}')
 
-
-
-        break
-
+            break
+        except ValueError:
+            print("Invalid gender. Please enter 'M' or 'F'.")
+    
 
 
 get_athlete_info()
